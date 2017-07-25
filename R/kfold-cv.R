ca3d1c93a0e914397afea1ddf20ab0b6dfcbabae
Cameron Roach <camroach87@gmail.com>
2017-07-25 19:42:52 +1000

    Add k-fold cross validation code

---
+++ R/kfold-cv.R
@@ -0,0 +1,36 @@
+#' K-fold cross validation
+#'
+#' Performs k-fold cross validation. A list with k elements is returned. Each
+#' element contains a train and test element, both of which are data frames.
+#'
+#' @param data a data frame containing relevant variables for modelling.
+#' @param k integer. Number of folds.
+#'
+#' @return
+#'
+#' `kfold_cv` returns a list of train and test data frames. Each element of the
+#' list corresponds to a fold.
+#'
+#' @export
+#'
+#' @examples
+#'
+#' kfold_data <- kfold_cv(iris, k = 5)
+#' head(kfold_data[[5]]$train)
+#' head(kfold_data[[5]]$test)
+kfold_cv <- function(data, k = 10) {
+  data<-data[sample(nrow(data)),]
+
+  folds <- cut(seq(1, nrow(data)),
+               breaks = k,
+               labels = FALSE)
+
+  fold_list <- list()
+  for(i in 1:k){
+    idx <- which(folds==i, arr.ind=TRUE)
+    fold_list[[i]] <- list("train" = data[-idx, ],
+                           "test" = data[idx, ])
+  }
+
+  return(fold_list)
+}
