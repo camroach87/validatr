e19687d48690ee56212f61c10d371b2777d5b184
Cameron Roach <camroach87@gmail.com>
2017-07-27 12:59:59 +1000

    Update accuracy measures script

--- R/accuracy-measures.R
+++ R/accuracy-measures.R
@@ -12,7 +12,7 @@
 #' * Symmetric mean absolute percentage error (SMAPE1 and SMAPE2 - see
 #' discussion)
 #'
-#' @param object an object containing cross-validation folds and predictions.
+#' @param .object a `validatr` object containing cross-validation folds and predictions.
 #' @param y string. Name of actual column.
 #' @param yhat string. Name of prediction column.
 #' @param average_folds logical. Should accuracy measures be averaged across all
@@ -24,27 +24,31 @@
 #' @export
 #'
 #' @examples
-#' kfold_data <- kfold_cv(iris)
-#' # TODO need to figure out how to do the prediction function, might need to be something the user codes since each prediction method is different.
-#' kfold_data <- predict(kfold_data)
-#' accuracy_measures(kfold_data, y = "Actual", yhat = "Prediction")
-accuracy_measures <- function(object, y, yhat, average_folds = TRUE) {
+#' kfold_cv(iris, k = 3) %>%
+#'   fit_models("Model1 = lm(Sepal.Length ~ ., data = train)",
+#'              "Model2 = lm(Sepal.Length ~ Sepal.Width + Petal.Width, data = train)") %>%
+#'   calc_predictions("Prediction1 = predict(Model1, newdata = test)",
+#'                    "Prediction2 = predict(Model2, newdata = test)") %>%
+#'   calc_accuracy(y = "Sepal.Length",
+#'                     yhat = c("Prediction1", "Prediction2"))
+calc_accuracy <- function(.object, y, yhat, average_folds = TRUE) {
   accuracy <- list()
-  for (i in 1:length(object)) {
-    # mean_naive_e <- object[[i]]$train %>%
+  for (i in 1:length(.object$folds)) {
+    # mean_naive_e <- .object[[i]]$train %>%
     #   dplyr::summarise(mean(abs(get(y) - lag(get(y))), na.rm = TRUE)) %>%
     #   dplyr::pull()
 
-    accuracy[[i]] <- object[[i]]$test %>%
+    accuracy[[i]] <- .object$folds[[i]]$test %>%
+      dplyr::select(y = y, yhat) %>%
+      tidyr::gather(Model, yhat, -y) %>%
+      dplyr::group_by(Model) %>%
       dplyr::summarise(
-        AE = sum(abs(get(y) - get(yhat)), na.rm = TRUE),
-        MAE = mean(abs(get(y) - get(yhat)), na.rm = TRUE),
-        MAPE = mean(abs(get(y) - get(yhat))/get(y), na.rm = TRUE)*100,
-        RMSE = sqrt(mean((get(y) - get(yhat))^2, na.rm = TRUE)),
-        SMAPE1 = mean(200*abs(get(y) - get(yhat))/(get(y) + get(yhat)),
-                      na.rm = TRUE),
-        SMAPE2 = mean(200*abs(get(y) - get(yhat))/abs(get(y) + get(yhat)),
-                      na.rm = TRUE)
+        AE = sum(abs(y - yhat), na.rm = TRUE),
+        MAE = mean(abs(y - yhat), na.rm = TRUE),
+        MAPE = mean(abs(y - yhat)/y, na.rm = TRUE)*100,
+        RMSE = sqrt(mean((y - yhat)^2, na.rm = TRUE)),
+        SMAPE1 = mean(200*abs(y - yhat)/(y + yhat), na.rm = TRUE),
+        SMAPE2 = mean(200*abs(y - yhat)/abs(y + yhat), na.rm = TRUE)
       ) %>%
       #              na.rm = TRUE),
       #MASE = AE/mean_naive_e) %>%
@@ -55,7 +59,15 @@ accuracy_measures <- function(object, y, yhat, average_folds = TRUE) {
   accuracy <- dplyr::bind_rows(accuracy)
 
   if (average_folds) {
-    accuracy <- colMeans(accuracy[,-1])
+    accuracy <- accuracy %>%
+      dplyr::select(-Fold) %>%
+      tidyr::gather(Measure, Accuracy, -Model) %>%
+      dplyr::group_by(Model, Measure) %>%
+      dplyr::summarise(Mean = mean(Accuracy),
+                       Variance = var(Accuracy)) %>%
+      tidyr::gather(Statistic, Value, -c(Model, Measure)) %>%
+      tidyr::spread(Measure, Value) %>%
+      dplyr::arrange(Statistic, Model)
   }
 
   return(accuracy)
