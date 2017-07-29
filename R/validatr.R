e15dca9db365d0bb228595a33fc66410de04983d
Cameron Roach <camroach87@gmail.com>
2017-07-29 13:57:42 +1000

    Changed kfold function to validatr function

---
+++ R/validatr.R
@@ -0,0 +1,78 @@
+#' Initialise validatr object
+#'
+#' Initialises a validatr object.
+#'
+#' The type of data being tested influences how the validatr and it's methods
+#' respond. The following types are supported and are specified using the type
+#' parameter.
+#'
+#' * __regression__ regression data.
+#' * __ts__ time-series data.
+#' * __classification__ classification data.
+#'
+#' The type of cross-validation and accuracy measures to be calculated are
+#' influenced by this parameter. For regression, k-fold cross-validation is
+#' carried out and requires the number of folds `k` to be specified. Leave one
+#' out cross-validation can easily be accomplished by setting `k` equal to the
+#' number of observations.
+#'
+#' For time-series, time-series cross-validation takes place. This requires the
+#' `start`, `horizon`, `jump` and `ts` arguments to be specified:
+#'
+#' * `start` is the start of the first fold.
+#' * `horizon` is the length of the fold.
+#' * `jump` is the length of time to move forward.
+#' * `ts` is the name of the variable containing time-series data.
+#'
+#' Finally, classification carries out k-fold cross validation as well, but its
+#' accruacy measures will be different to regression.
+#'
+#' @param data data frame containing variables for modelling.
+#' @param type string specify the data structure and cross-validation to be
+#'   carried out. Can be one of ts, regression or classification.
+#' @param k integer. Number of folds.
+#'
+#' @return
+#'
+#' `validatr` returns an initial validatr object. This object contains cross
+#' validation folds and validation parameters.
+#'
+#' @export
+#'
+#' @examples
+#'
+#' validatr_obj <- validatr(iris, data_type = "regression", k = 5)
+#' head(validatr_obj$folds[[5]]$train)
+#' head(validatr_obj$folds[[5]]$validation)
+validatr <- function(data,
+                     data_type = "regression",
+                     k = 10,
+                     start = NULL,
+                     horizon = NULL,
+                     jump = NULL,
+                     ts = NULL) {
+
+  validatr <- list(params = as.list(environment()),
+                   folds = list())
+
+  if (data_type %in% c("regression", "classification")) {
+    data<-data[sample(nrow(data)),]
+
+    folds <- cut(seq(1, nrow(data)),
+                 breaks = k,
+                 labels = FALSE)
+
+    for(i in 1:k){
+      idx <- which(folds==i, arr.ind=TRUE)
+      validatr$folds[[i]] <- list("train" = data[-idx, ],
+                                  "validation" = data[idx, ])
+    }} else if (data_type == "ts") {
+      stop("ts not implemented yet")
+    } else {
+      stop("Invalid data_type entered.")
+    }
+
+  class(validatr) <- "validatr"
+
+  return(validatr)
+}
