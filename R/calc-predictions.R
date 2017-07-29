cd3c36c5bbf2a082e25fb4745c48d051d39e84d5
Cameron Roach <camroach87@gmail.com>
2017-07-29 17:13:52 +1000

    Update calc-predictions to handle validatr object.

--- R/calc-predictions.R
+++ R/calc-predictions.R
@@ -1,21 +1,17 @@
 #' Predict on validation set
 #'
-#' __TODO: at the moment the prediction strings for each model must be in the
-#' same order as in the__ `fit_models` __function. Fix so that order does not
-#' need to be preserved to avoid potential bugs. Might need to learn a bit more
-#' about NSE.__
-#'
 #' Carries out prediction on validation set for each model.
 #'
 #' @param .object a `validatr`` object produced by `fit_models`.
-#' @param ... strings specifying what predictions are needed.
+#' @param ... strings specifying what predictions are needed. Each string, if
+#'   evaluated, should return a vector of predictions.
 #'
 #' @return
 #' @export
 #'
 #' @examples
 #'
-#' kfold_cv(iris, k = 3) %>%
+#' validatr(iris, k = 3) %>%
 #'   fit_models(Model1 = lm(Sepal.Length ~ ., data = train),
 #'              Model2 = lm(Sepal.Length ~ Sepal.Width + Petal.Width, data = train)) %>%
 #'   calc_predictions(Model1 = predict(Model1, newdata = validation),
@@ -28,7 +24,7 @@ calc_predictions <- function(.object, ...) {
 calc_predictions.validatr <- function(.object, ...) {
   predict_spec <- eval(substitute(alist(...)))
 
-  for (iF in 1:length(.object$folds)) {
+  for (iF in names(.object$folds)) {
     for (iP in names(predict_spec)) {
       assign(iP, .object$models[[iF]][[iP]])
       validation <- .object$folds[[iF]]$validation
