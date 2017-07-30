bc5816fb20b8e9d5da29f1526dbf56189febebf9
Cameron Roach <camroach87@gmail.com>
2017-07-30 17:04:59 +1000

    Fix autoplot function

--- R/autoplot.R
+++ R/autoplot.R
@@ -11,17 +11,22 @@
 #'
 #' @examples
 #' validatr(iris, k = 3) %>%
-#'   fit_models(Model1 = lm(Sepal.Length ~ ., data = train),
-#'              Model2 = lm(Sepal.Length ~ Sepal.Width + Petal.Width, data = train)) %>%
-#'   calc_predictions(Model1 = predict(Model1, newdata = validation),
-#'                    Model2 = predict(Model2, newdata = validation)) %>%
+#'   fit_models(LM1 = lm(Sepal.Length ~ ., data = train),
+#'              LM2 = lm(Sepal.Length ~ Sepal.Width + Petal.Width, data = train)) %>%
+#'   calc_predictions(LM1 = predict(LM1, newdata = validation),
+#'                    LM2 = predict(LM2, newdata = validation)) %>%
 #'   calc_accuracy(y = "Sepal.Length", average_folds = FALSE) %>%
 #'   autoplot()
+autoplot <- function(object, ...) {
+  UseMethod("autoplot")
+}
+
+#' @export
 autoplot.validatr_accuracy <- function(object, ...) {
   object %>%
     tidyr::gather(Measure, Accuracy, -c(Fold, Model)) %>%
     ggplot2::ggplot(ggplot2::aes(x = Accuracy, fill = Model)) +
     ggplot2::geom_density(alpha = 0.3) +
-    ggplot2::facet_wrap(~Measure, scales = "free_x") +
+    ggplot2::facet_wrap(~Measure, scales = "free") +
     ggplot2::labs(title = "Accuracy measures for cross-validation folds")
 }
