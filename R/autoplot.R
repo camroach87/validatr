a8a6de94c119e4c7f35cf7aa4bea45fcb08cac1e
Cameron Roach <camroach87@gmail.com>
2017-07-31 22:55:18 +1000

    Change accuracy plots to violin plots.

--- R/autoplot.R
+++ R/autoplot.R
@@ -1,5 +1,8 @@
 #' Plot accuracy measure densities
 #'
+#' Produces violin plots for accuracy measures. Jittered points are also used
+#' to show actual values.
+#'
 #' @param object a validatr object produced by `assess()`.
 #' @param ...
 #'
@@ -25,8 +28,12 @@ autoplot <- function(object, ...) {
 autoplot.validatr <- function(object, ...) {
   object$accuracy$fold_accuracy %>%
     tidyr::gather(Measure, Accuracy, -c(Fold, Model)) %>%
-    ggplot2::ggplot(ggplot2::aes(x = Accuracy, fill = Model)) +
-    ggplot2::geom_density(alpha = 0.3) +
-    ggplot2::facet_wrap(~Measure, scales = "free") +
-    ggplot2::labs(title = "Accuracy measures for cross-validation folds")
+    ggplot2::ggplot(ggplot2::aes(x = Model, y = Accuracy,
+                                 fill = Model)) +
+    ggplot2::geom_violin(alpha = 0.3, colour = NA) +
+    ggplot2::geom_jitter(width = 0.1, size = 0.15) +
+    ggplot2::facet_wrap(~Measure, scales = "free_x") +
+    ggplot2::labs(title = "Accuracy measures for cross-validation folds") +
+    ggplot2::theme(legend.position = "none") +
+    ggplot2::coord_flip()
 }
